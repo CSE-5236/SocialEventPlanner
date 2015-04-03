53303b70cc1b2cba15be76a5952b7cbb8c181b04
abenson91 <benson.307@osu.edu>
2015-04-02 21:41:03 -0400

    Fixed Commit Errors
    I messed up the AndroidManifest making a merge and this update fixes it
    and adds the user class

---
+++ app/src/main/java/cse5236/group11/socialeventplanner/User.java
@@ -0,0 +1,61 @@
+package cse5236.group11.socialeventplanner;
+
+/**
+ * Created by Aaron on 4/2/2015.
+ */
+public class User {
+    private String username;
+    private int id;
+    private String email; //might make this the same as username
+    private long phoneNumber;
+    private String firstName;
+    private String lastName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
